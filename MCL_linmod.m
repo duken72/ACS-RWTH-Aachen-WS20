55dbb9bb32d9bf3ad117419054f0ed8bdb16c368
Duke Nguyen <duke7295@gmail.com>
2021-02-12 12:02:28 +0100

    Update MCL_linmod.m

--- MCL_linmod.m
+++ MCL_linmod.m
@@ -3,9 +3,6 @@
 % and transfers it into the state-space linearised model  
 % and into the transfer function matrix 
 
-clear all;
-clc;
-
 %% init all parameters
 MCL_init;
 
@@ -52,20 +49,17 @@ set(op.outputs(4), 'Min', -20);       % limit P_pc2 [mmHG]
 set(op.outputs(4), 'Max', 100);       % limit P_pc2 [mmHG]
 
 op.outputs(5).Description='F_GP1';
-set(op.outputs(5), 'Min', 0);        % limit F_GP1 [mmHG]
-set(op.outputs(5), 'Max', 100);        % limit F_GP1 [mmHG]
+set(op.outputs(5), 'Min', 0);         % limit F_GP1 [mmHG]
+set(op.outputs(5), 'Max', 100);       % limit F_GP1 [mmHG]
  op.outputs(5).y=3;                   % F_GP1 [L/min]
 
 % Speed of rotation
-op.states(4).Known = 1; %Duke
-op.states(2).Known = 1; %Duke
-op.states(13).Known = 1; %Duke
-op.states(4).SteadyState = 1; %Duke
-op.states(2).SteadyState = 1; %Duke
-op.states(13).SteadyState = 1; %Duke
-op.states(4).x=700*(1/RADS2TURNMIN);  % w_GP1 %Dipankar
-op.states(2).x=500*(1/RADS2TURNMIN);  % w_GP12 %Dipankar
-op.states(13).x=500*(1/RADS2TURNMIN); % w_GP2 %Dipankar
+op.states(4).Known = 1;
+op.states(2).Known = 1;
+op.states(13).Known = 1;
+op.states(4).x=700*(1/RADS2TURNMIN);  % w_GP1
+op.states(2).x=500*(1/RADS2TURNMIN);  % w_GP12
+op.states(13).x=500*(1/RADS2TURNMIN); % w_GP2
 
 % Limit states/set state conditions
 set(op.states(1), 'Min', -7.5);   % Limit i_GP12 [A]
@@ -87,12 +81,10 @@ set(op.states(10), 'Max', 0.0137) % limit x_VCA1 [m]
 set(op.states(19), 'Min', 0);     % limit x_VCA2 [m]
 set(op.states(19), 'Max', 0.0137) % limit x_VCA2 [m]
 
-%opt = findopOptions; %Duke
-opt = linoptions; %Dipankar
+opt = findopOptions;
 opt.DisplayReport='on';
-%opt.OptimizerType='graddescent-elim'; %Duke
-opt.OptimizerType='lsqnonlin'; %Dipankar
-opt.OptimizationOptions.Algorithm= 'trust-region-reflective'; %Dipankar
+opt.OptimizerType='lsqnonlin';
+opt.OptimizationOptions.Algorithm= 'trust-region-reflective';
 opt.OptimizationOptions.DiffMaxChange = 0.1;
 opt.OptimizationOptions.MaxIter = 5000;
 opt.OptimizationOptions.MaxFunEvals = 1000;
@@ -109,6 +101,9 @@ X  = [op_report.states(1).x; op_report.states(2).x; op_report.states(3).x; op_re
 Y  = [op_report.outputs(1).y; op_report.outputs(2).y; op_report.outputs(3).y; op_report.outputs(4).y; op_report.outputs(5).y];
 
 %% init the MCL model from Simulink
+op_point_new = load('MCL_op.mat');
+X = cell2mat(get(op_point_new.op.States,'x'));
+U = cell2mat(get(op_point_new.op.Input,'u'));
 
 % Load state-space data from MCL simulink model
 [Al, Bl, Cl, Dl]=linmod('MCL', X, U);
@@ -119,16 +114,20 @@ set(Sys,'inputname',[{'U\_GP1'} {'U\_VCA1'} {'U\_GP12'} {'U\_GP2'} {'U\_VCA2'}],
 
 %% Task 3.1.1. The stable operating point
 fprintf('\nTask 3.1.1. The stable operating point');
-format shortG; %Duke
+format shortG;
 display(U);
 display(Y)
-%display(X([4, 2, 13],1)*RADS2TURNMIN); %Duke
+display(X([4, 2, 13],1)*RADS2TURNMIN);
+display(DX);
+if sum(DX) ~= 0
+    fprintf('We do not have a STABLE operating point (DX not equal 0).\n');
+end
 
 %% Task 3.1.2. Analysis of the linearised model
 fprintf('Task 3.1.2. Analysis of the linearised model\n');
 fprintf('The dimension of the system matrix is: %g.\n', length(Al));
-[b,a] = ss2tf(Al,Bl,Cl,Dl,5); %Have not remove 0 elements of b
-fprintf('The order of the tf matrix of the system is: %g.\n', max(max(size(a)),max(size(b)))-1);
+TFSys = tf(Sys);
+fprintf('The order of the tf matrix of the system is: %g.\n', order(Sys));
 
 polesSys = pole(Sys);
 fprintf('Number of unstable poles: %g.\n', size(polesSys(real(polesSys)>0,:),1));
@@ -155,46 +154,50 @@ h = hsvplot(Sys);
 
 %% Task 3.2.2. Reduce the model
 fprintf('Task 3.2.2. Reduce the model\n');
-[redSys3] = balred(Sys,3);
-[redSys5] = balred(Sys,5);
-[redSys10] = balred(Sys,10);
-[redSys11] = balred(Sys,11);
-[redSys12] = balred(Sys,12);
-
-t = 0:0.1:10;
-s0 = step(Sys,t);
-s3 = step(redSys3,t);
-s10 = step(redSys10,t);
-s11 = step(redSys11,t);
-s12 = step(redSys12,t);
+[SysS, SysU] = stabsep(ssbal(Sys));
+hsvs = hsvd(SysS);
 
 figure(2);
-i=4; j=4;
-%plot(s0(:,i,j),'DisplayName','original system');
-plot(s0(:,i,j)-s3(:,i,j),'DisplayName','redSys3'); hold on;
-plot(s0(:,i,j)-s10(:,i,j),'DisplayName','redSys10');
-plot(s0(:,i,j)-s11(:,i,j),'DisplayName','redSys11');
-plot(s0(:,i,j)-s12(:,i,j),'DisplayName','redSys12');
+hsvplot(SysS);
+
+% Reduce the stable model part
+redOrder = 5;
+[redSysS] = balred(SysS,redOrder);
+
+fprintf('The order of the reduced stable model : %g.\n\n', order(redSysS));
+figure(3);
+step(SysS,'r',redSysS,'g--')
 legend;
 xlabel('time'); ylabel('response');
 
-[b_red,a_red] = ss2tf(redSys12.a,redSys12.b,redSys12.c,redSys12.d,5);
-fprintf('The order of the reduced model: %g.\n\n', max(max(size(a_red)),max(size(b_red)))-1);
-
+%% Combine the reduced stable part and the unstable part
+orderRedSysU = length(SysU.A);
+redA = [redSysS.A,                    zeros(redOrder,orderRedSysU);
+        zeros(orderRedSysU,redOrder), SysU.A];
+redB = [redSysS.B; SysU.B];
+redC = [redSysS.C, SysU.C];
+redD = redSysS.D + SysU.D;
+redSys = ss(redA, redB, redC, redD);
+figure(4);
+step(Sys,'r',redSys,'g--')
+legend;
+xlabel('time'); ylabel('response');
 %% Task 3.2.3. Minimal realisation
-fprintf('Task 3.2.3. Minimal realisation');
-minSys = minreal(redSys3, tol);
+fprintf('Task 3.2.3. Minimal realisation\n');
+minSys = minreal(redSysS, tol);
 display(minSys);
 
 %% Task 3.2.4. RGA
 fprintf('Task 3.2.4. Static Relative Gain Array');
-minG = zeros(5);
+minG = tf(minSys);
+minsG = zeros(5);
 for i=1:5
-    [minA, minB] = ss2tf(minSys.a, minSys.b, minSys.c, minSys.d, i);
-    minG(:,i) = minA(:,1) / minB(1);
+    for j=1:5
+        minsG(i,j) = minG.Numerator{i,j}(end) / minG.Denominator{i,j}(end);
+    end
 end
-sRGA = minG.*pinv(minG).';
-rsRGA = round(sRGA,2);
+sRGA = minsG.*pinv(minsG).';
+rsRGA = round(sRGA,3);
 display(rsRGA);
 
 %% Task 3.2.5. Scaling
@@ -206,11 +209,9 @@ Dd = eye(5);
 scaled_G = pinv(De) * minG * Du;
 display(scaled_G);
 
-%% Task 3.3.1. Sources for uncertainty
-fprintf('Task 3.3.1. Sources for uncertainty\n\n');
-
 %% Task 3.3.2. Weighting functions for uncertainty
 fprintf('Task 3.3.2. Weighting functions for uncertainty\n\n');
+
 
 %% Task 3.3.3. Visualize uncertain system
 fprintf('Task 3.3.3. Visualize uncertain system\n\n');
